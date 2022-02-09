65c8cb105861f79036f71a39718904f29624ecab
kondrei <kondrei@gmail.com>
2022-02-09 15:40:56 +0200

    refactoring code to OOP

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
-let clock = document.querySelector('.clock')
-function showClock() {
-    let currentTime = new Date()
-    clock.textContent = addZero(currentTime.getHours()) + ':' + addZero(currentTime.getMinutes())
-}
-function addZero(time) {
-    return (time < 10) ? '0' + time : time
-}
-showClock()
-setInterval(showClock, 40000)
 
-let keys = [
+class Calculator {
+    /*
+    * Class Calculator
+    * @param {HTML Element} output = element where calculation will be shown
+    *    
+    *  @returns (string) element.textContent
+    */
+    constructor(output) {
+        this.output = output
+
+        this.showClock()
+        this.reset()
+    }
+
+
+
+    showClock() {
+        let clock = document.querySelector('.clock')
+        function addZero(time) {
+            return (time < 10) ? '0' + time : time
+        }
+        function displayClock() {
+            let currentTime = new Date()
+            clock.textContent = addZero(currentTime.getHours()) + ':' + addZero(currentTime.getMinutes())
+        }
+        displayClock()
+        setInterval(displayClock, 1000)
+    }
+
+    reset() {
+        this.output.textContent = 0
+        this.leftOperand = 0
+        this.rightOperand = 0
+        this.operand = null
+        this.firstIOperation = null
+        console.log('reset method')
+    }
+
+    del() {
+        let digitRemains = this.output.textContent.split('')
+        digitRemains.pop();
+        (digitRemains.join('')) ? output.textContent = digitRemains.join('') : output.textContent = '0';
+        (this.operand) ? this.rightOperand = this.output.textContent : this.eftOperand = this.output.textContent
+    }
+}
+
+
+this.keys = [
     { type: 'reset', text: 'AC', color: 'gray' },
     { type: 'del', text: '&#8592;', color: 'gray' },
     { type: 'percent', text: '%', color: 'gray' },
@@ -32,13 +69,9 @@ let keys = [
 ]
 
 let buttons = document.querySelector('.buttons')
-let output = document.querySelector('.outputDisplay')
 
-let leftOperand = 0;
-let rightOperand = 0;
-let operand = null;
-let firstIOperation = null;
-
+output = document.querySelector('.outputDisplay')
+let calculator = new Calculator(output)
 
 for (let key in keys) {
     let button = document.createElement('div')
@@ -49,135 +82,213 @@ for (let key in keys) {
         button.classList.add('zero')
     }
     button.addEventListener('click', (e) => {
-        buttonsEvents(e, keys[key]);
+        //buttonsEvents(e, keys[key]);
+        switch (keys[key].type) {
+            case 'reset':
+                calculator.reset()
+                break;
+            case 'del':
+                calculator.del()
+                break;
+            case 'comma':
+                if (this.output.textContent.indexOf(keys[key].text) < 0) {
+                    this.output.textContent += keys[key].text
+                }
+                //(operand) ? rightOperand += object.text : leftOperand += keys[key]
+                break;
+            default:
+                break;
+        }
     })
     buttons.appendChild(button)
 }
 
-function allClear() {
-    output.textContent = 0
-    leftOperand = 0
-    rightOperand = 0
-    operand = null
-    document.querySelectorAll('.selected').forEach((element) => {
-        element.classList.remove('selected')
-    })
-}
 
 
-function buttonsEvents(e, object) {
+// let clock = document.querySelector('.clock')
+// function showClock() {
+//     let currentTime = new Date()
+//     clock.textContent = addZero(currentTime.getHours()) + ':' + addZero(currentTime.getMinutes())
+// }
+// function addZero(time) {
+//     return (time < 10) ? '0' + time : time
+// }
+// showClock()
+// setInterval(showClock, 40000)
 
-    if (object.type === 'reset') {
-        allClear()
-    }
+// let keys = [
+//     { type: 'reset', text: 'AC', color: 'gray' },
+//     { type: 'del', text: '&#8592;', color: 'gray' },
+//     { type: 'percent', text: '%', color: 'gray' },
+//     { type: 'operand', text: '&#247;', color: 'orange' },
+//     { type: 'number', text: '7', color: 'black' },
+//     { type: 'number', text: '8', color: 'black' },
+//     { type: 'number', text: '9', color: 'black' },
+//     { type: 'operand', text: 'x', color: 'orange' },
+//     { type: 'number', text: '4', color: 'black' },
+//     { type: 'number', text: '5', color: 'black' },
+//     { type: 'number', text: '6', color: 'black' },
+//     { type: 'operand', text: '-', color: 'orange' },
+//     { type: 'number', text: '1', color: 'black' },
+//     { type: 'number', text: '2', color: 'black' },
+//     { type: 'number', text: '3', color: 'black' },
+//     { type: 'operand', text: '+', color: 'orange' },
+//     { type: 'number', text: '0', color: 'black' },
+//     { type: 'comma', text: '.', color: 'black' },
+//     { type: 'equal', text: '=', color: 'orange' },
+// ]
 
-    if (object.type === 'del') {
-        let digitRemains = output.textContent.split('')
-        digitRemains.pop();
-        (digitRemains.join('')) ? output.textContent = digitRemains.join('') : output.textContent = '0';
-        (operand) ? rightOperand = output.textContent : leftOperand = output.textContent
-    }
+// let buttons = document.querySelector('.buttons')
+// let output = document.querySelector('.outputDisplay')
 
-    if (object.type === 'number' && checkLength(output.textContent)) {
-        (operand) ? rightOperand += object.text : leftOperand += object.text
+// let leftOperand = 0;
+// let rightOperand = 0;
+// let operand = null;
+// let firstIOperation = null;
 
-        if (firstIOperation) {
-            output.textContent = object.text
-            firstIOperation = false
-        } else
-            (output.textContent === '0') ? output.textContent = object.text : output.textContent += object.text
-    }
 
-    if (object.type === 'comma') {
-        if (output.textContent.indexOf(object.text) < 0) {
-            output.textContent += object.text
-        }
-        (operand) ? rightOperand += object.text : leftOperand += object.text
-    }
+// for (let key in keys) {
+//     let button = document.createElement('div')
+//     button.classList.add('button')
+//     button.classList.add(keys[key].color)
+//     button.innerHTML = keys[key].text
+//     if (keys[key].text === '0') {
+//         button.classList.add('zero')
+//     }
+//     button.addEventListener('click', (e) => {
+//         buttonsEvents(e, keys[key]);
+//     })
+//     buttons.appendChild(button)
+// }
 
-    if (object.type === 'operand' && leftOperand != 0) {
-        e.target.classList.add('selected')
-        operand = object.text
-        firstIOperation = true
-        if (leftOperand != 0 && rightOperand != 0 && operand) {
-            output.textContent = calculate(leftOperand, rightOperand, operand)
-        }
-    }
+// function allClear() {
+//     output.textContent = 0
+//     leftOperand = 0
+//     rightOperand = 0
+//     operand = null
+//     document.querySelectorAll('.selected').forEach((element) => {
+//         element.classList.remove('selected')
+//     })
+// }
 
-    if (object.type === 'percent') {
-        output.textContent = '0.' + output.textContent;
-        (operand) ? rightOperand = output.textContent : leftOperand = output.textContent
-    }
 
-    if (object.type === 'equal' && leftOperand != 0 && rightOperand != 0 && operand) {
-        output.textContent = calculate(leftOperand, rightOperand, operand)
-    }
-    output.textContent = formatDigits(output.textContent)
-    console.log(leftOperand, operand, rightOperand)
-}
+// function buttonsEvents(e, object) {
 
-function calculate(left, right, operation) {
-    switch (operation) {
-        case "+":
-            result = parseFloat(left) + parseFloat(right)
-            break;
-        case "-":
-            result = parseFloat(left) - parseFloat(right)
-            break;
-        case "x":
-            result = parseFloat(left) * parseFloat(right)
-            break;
-        case "&#247;":
-            result = parseFloat(left) / parseFloat(right)
-            break;
-    }
-    allClear()
-    leftOperand = result
-    return result
-}
+//     if (object.type === 'reset') {
+//         allClear()
+//     }
 
-function formatDigits(number) {
-    number = formatNumber(number)
-    if (parseFloat(number) > 999999) {
-        output.classList.add('small')
-    } else {
-        output.classList.remove('small')
-    }
+//     if (object.type === 'del') {
+//         let digitRemains = output.textContent.split('')
+//         digitRemains.pop();
+//         (digitRemains.join('')) ? output.textContent = digitRemains.join('') : output.textContent = '0';
+//         (operand) ? rightOperand = output.textContent : leftOperand = output.textContent
+//     }
 
-    return checkComma(number)
-}
+//     if (object.type === 'number' && checkLength(output.textContent)) {
+//         (operand) ? rightOperand += object.text : leftOperand += object.text
 
-function checkComma(number) {
-    if (number[number.length - 1] === '.') {
-        number = parseFloat(number).toLocaleString()
-        return number + '.'
-    } else {
-        return parseFloat(number).toLocaleString()
-    }
-}
+//         if (firstIOperation) {
+//             output.textContent = object.text
+//             firstIOperation = false
+//         } else
+//             (output.textContent === '0') ? output.textContent = object.text : output.textContent += object.text
+//     }
 
-function checkLength(number) {
-    number = formatNumber(number)
-    if (number.length > 10) {
-        return false
-    }
+//     if (object.type === 'comma') {
+//         if (output.textContent.indexOf(object.text) < 0) {
+//             output.textContent += object.text
+//         }
+//         (operand) ? rightOperand += object.text : leftOperand += object.text
+//     }
 
-    return true;
-}
+//     if (object.type === 'operand' && leftOperand != 0) {
+//         e.target.classList.add('selected')
+//         operand = object.text
+//         firstIOperation = true
+//         if (leftOperand != 0 && rightOperand != 0 && operand) {
+//             output.textContent = calculate(leftOperand, rightOperand, operand)
+//         }
+//     }
 
-function formatNumber(number) {
-    return number.replaceAll(',', '')
-}
+//     if (object.type === 'percent') {
+//         output.textContent = '0.' + output.textContent;
+//         (operand) ? rightOperand = output.textContent : leftOperand = output.textContent
+//     }
 
-document.addEventListener('keyup', (e) => {
-    switch (e.key) {
-        case 'Escape':
-            allClear()
-            break;
-        case '?':
-            document.querySelector('.background').classList.toggle('show')
-            break;
-        default:
-            break;
-    }
-})
+//     if (object.type === 'equal' && leftOperand != 0 && rightOperand != 0 && operand) {
+//         output.textContent = calculate(leftOperand, rightOperand, operand)
+//     }
+//     output.textContent = formatDigits(output.textContent)
+//     console.log(leftOperand, operand, rightOperand)
+// }
+
+// function calculate(left, right, operation) {
+//     switch (operation) {
+//         case "+":
+//             result = parseFloat(left) + parseFloat(right)
+//             break;
+//         case "-":
+//             result = parseFloat(left) - parseFloat(right)
+//             break;
+//         case "x":
+//             result = parseFloat(left) * parseFloat(right)
+//             break;
+//         case "&#247;":
+//             result = parseFloat(left) / parseFloat(right)
+//             break;
+//     }
+//     allClear()
+//     leftOperand = result
+//     return result
+// }
+
+// function formatDigits(number) {
+//     number = formatNumber(number)
+//     if (parseFloat(number) > 999999) {
+//         output.classList.add('small')
+//     } else {
+//         output.classList.remove('small')
+//     }
+
+//     return checkComma(number)
+// }
+
+// function checkComma(number) {
+//     if (number[number.length - 1] === '.') {
+//         number = parseFloat(number).toLocaleString()
+//         return number + '.'
+//     } else {
+//         return parseFloat(number).toLocaleString()
+//     }
+// }
+
+// function checkLength(number) {
+//     number = formatNumber(number)
+//     if (number.length > 10) {
+//         return false
+//     }
+
+//     return true;
+// }
+
+// function formatNumber(number) {
+//     return number.replaceAll(',', '')
+// }
+
+// document.querySelector('.background').addEventListener('click', () => {
+//     document.querySelector('.background').classList.remove('show')
+// })
+
+// document.addEventListener('keyup', (e) => {
+//     switch (e.key) {
+//         case 'Escape':
+//             allClear()
+//             break;
+//         case '?':
+//             document.querySelector('.background').classList.toggle('show')
+//             break;
+//         default:
+//             break;
+//     }
+// })
